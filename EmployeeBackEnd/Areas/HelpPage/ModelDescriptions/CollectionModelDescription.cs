7a02a3ea8647892592349a269cf9868ab897c9a5
Harsha192 <harshagayan8@gmail.com>
2018-06-14 00:03:46 +0530

    first commit

---
+++ EmployeeBackEnd/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace EmployeeBackEnd.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ EmployeeBackEnd/Controllers/EmployeeController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using EmployeeBackEnd.Models;
+using System.Web.Http.Cors;
+
+namespace EmployeeBackEnd.Controllers
+{
+    //[EnableCors(origins: "http://localhost:4200/", headers: "*", methods: "*" )]
+    public class EmployeeController : ApiController
+    {
+        private EmployeeDetailsEntities db = new EmployeeDetailsEntities();
+
+        // GET: api/Employee
+        public IQueryable<Table> GetTables()
+        {
+            return db.Tables;
+        }
+
+        // GET: api/Employee/5
+        [ResponseType(typeof(Table))]
+        public IHttpActionResult GetTable(int id)
+        {
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(table);
+        }
+
+        // PUT: api/Employee/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTable(int id, Table table)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != table.EmployeeID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(table).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TableExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Employee
+        [ResponseType(typeof(Table))]
+        public IHttpActionResult PostTable(Table table)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Tables.Add(table);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = table.EmployeeID }, table);
+        }
+
+        // DELETE: api/Employee/5
+        [ResponseType(typeof(Table))]
+        public IHttpActionResult DeleteTable(int id)
+        {
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            db.Tables.Remove(table);
+            db.SaveChanges();
+
+            return Ok(table);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TableExists(int id)
+        {
+            return db.Tables.Count(e => e.EmployeeID == id) > 0;
+        }
+    }
+}
